2b074aba82f71ec86c50f0d034011a7662b78986
gokul-krishna <gg@usfca.edu>
2019-01-02 00:20:05 -0800

    added confusion matrix plot function

--- plotting.py
+++ plotting.py
@@ -1,6 +1,7 @@
 from .basic import *
-import math
 from PIL import Image
+
+import itertools
 
 # set styling options similar to R's ggplot
 plt.style.use('ggplot')
@@ -44,3 +45,25 @@ def save_spectrogram(data, fpath, log_col=False, log_freq=False):
     fig.savefig(fpath, dpi=100, frameon='false')
     plt.close(fig)
     return True
+
+
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], 'd'),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
